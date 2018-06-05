743e88e0e15924e68cdc10004f01dd617c7cb128
rSage <rodrigo@sagebin.com>
2018-06-05 12:19:51 -0600

    Deletion of original VC file and adding of the "PrincipleVC" file along with connecting it to the storyboard

---
+++ kiDoku/kiDoku/View Controllers/PrincipleViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PrincipleViewController.swift
+//  kiDoku
+//
+//  Created by Rodrigo Sagebin on 6/5/18.
+//  Copyright © 2018 Rodrigo Sagebin. All rights reserved.
+//
+
+import UIKit
+
+class PrincipleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
